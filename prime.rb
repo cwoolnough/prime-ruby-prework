c185d89f1752866a8be58c81269974822f3c5431
cwoolnoughchamorro <cwoolnoughchamorro@googlemail.com>
2018-09-18 14:51:12 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,13 @@
-def prime(integer)
-  integer = gets.to_i
+def prime?(num)
+  if num < 2
+    return false
+  end
+  i = 2
+  while i <= num / 2
+    if num % i == 0
+      return false
+    end
+    i += 1
+  end
+  return true
 end
